06be105a403d409a687ce8ab7729a6eb34aec732
Luis Rey Lara <luisreylara@hotmail.com>
2025-06-06 11:15:55 -0600

    Update ejm1.sql

--- docker-compose/LAMP/Practica1/base_datos/ejm1.sql
+++ docker-compose/LAMP/Practica1/base_datos/ejm1.sql
@@ -1,8 +1,8 @@
-FROM php:8.0.0-apache
-ARG DEBIAN_FRONTEND=noninteractive
-RUN docker-php-ext-install mysqli
-RUN apt update \
-    && apt install libzip-dev zlib1g-dev -y \
-    && rm -rf /var/lib/apt/lists/* \
-    && docker-php-ext-install zip
-RUN a2enmod rewrite
+CREATE TABLE Usuarios(
+    id int(11) NOT NULL,
+    nombre varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+INSERT INTO Usuarios(id, nombre) VALUES (435, 'Juan Ramirez');
+INSERT INTO Usuarios(id, nombre) VALUES (552, 'Ana Perez');
+INSERT INTO Usuarios(id, nombre) VALUES (125, 'Miguel Torrez');
+INSERT INTO Usuarios(id, nombre) VALUES (999, 'Sofia Pereira');
